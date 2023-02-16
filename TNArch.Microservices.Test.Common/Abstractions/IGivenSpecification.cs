8882b4584c812cf3761dcf620f83cc228c4f4626
Tibor Nagy <Tibor.Nagy@newsignature.com>
2023-02-16 14:05:33 +0200

    Add UnitTestSpecification

---
+++ TNArch.Microservices.Test.Common/Abstractions/IGivenSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+
+namespace TNArch.Microservices.Test.Common.Abstractions
+{
+    public interface IGivenSpecification<T> where T : class
+    {
+        IGivenSpecification<T> GivenMock<TMock>(Action<Mock<TMock>> given) where TMock : class;
+        IGivenSpecification<T> GivenMock<TMock1, TMock2>(Action<Mock<TMock1>, Mock<TMock2>> given) where TMock1 : class where TMock2 : class;
+        IGivenSpecification<T> GivenDependency<TContract, TService>() where TContract : class where TService : class, TContract;
+        IGivenSpecification<T> GivenDependency<TService>(TService instance);
+        IGivenSpecification<T> GivenExpectedException<TException>() where TException : Exception;
+        IGivenSpecification<T> GivenLogging();
+        IPartialMockedGivenSpecification<T> GivenPartialMock(Action<Mock<T>> given);
+        IPartialMockedGivenSpecification<T> GivenPartialMock<TMock>(Action<Mock<T>, Mock<TMock>> given) where TMock : class;
+        IThenSpecification<T> WhenExecute(Action<T> action);
+        IThenSpecificationWithResult<T, TResult> WhenExecute<TResult>(Func<T, TResult> action);
+        IThenSpecification<T> WhenInitialized();
+    }
+}
---
+++ TNArch.Microservices.Test.Common/Specification.cs
@@ -0,0 +1,12 @@
+﻿using TNArch.Microservices.Test.Common.Abstractions;
+
+namespace TNArch.Microservices.Test.Common
+{
+    public static class Specification
+    {
+        public static IGivenSpecification<T> ForService<T>() where T : class
+        {
+            return new ServiceSpecification<T>();
+        }
+    }
+}
